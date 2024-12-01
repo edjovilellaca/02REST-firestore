b07fc49b80ac5202f236a5e716fa0e9f3e20529e
Joel/Zakloni <edjovilellaca@ittepic.edu.mx>
2024-11-30 19:35:18 -0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,7 +7,7 @@ const { getFirestore  } = require('firebase/firestore');
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "ApiKey", //pa que se quede secreta pq chance y la uso luego
+  apiKey: "AIzaSyB3yZH-mN7OMt0PcB7OBQgmh53e0aEuigE", //pa que se quede secreta pq chance y la uso luego
   authDomain: "rest-serviciosweb.firebaseapp.com",
   projectId: "rest-serviciosweb",
   storageBucket: "rest-serviciosweb.appspot.com",
